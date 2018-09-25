80fb9a1aebc6bc70ba3143d9da339f976a604cd6
Christopher Doyle <christopher.doyle@performgroup.com>
2018-09-25 15:27:38 +0100

    modify: add the cards in correct sequence initially

--- app/Http/Controllers/DeckController.php
+++ app/Http/Controllers/DeckController.php
@@ -9,14 +9,15 @@ use Illuminate\Support\Arr;
 
 class DeckController extends Controller {
 
-    /*public function DealCards () {
-        return view('app');
-    }*/
-
-    public function index(Cards $cards)
-    {
+    public function index (Cards $cards) {
         $shuffledDeck = Arr::shuffle($cards->deckOfCards());
-        return view('DealCards', compact('shuffledDeck'));
+
+        return view('DealCards')
+            ->with('deckOfCards', $cards->deckOfCards())
+            ->with('shuffledDeck', $shuffledDeck)
+            ;
+
+        //return view('DealCards', compact('shuffledDeck'));
     }
 
 }
--- resources/views/DealCards.blade.php
+++ resources/views/DealCards.blade.php
@@ -4,6 +4,14 @@
 
     <h1>Deal Cards</h1>
 
+    <h2>Straight Deck</h2>
+
+    @foreach($deckOfCards as $deck)
+        {!! $deck !!}
+    @endforeach
+
+    <h2>Shuffled Deck</h2>
+
     @foreach($shuffledDeck as $shuffled)
         {!! $shuffled !!}
     @endforeach
